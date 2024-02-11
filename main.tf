db6cd835a42a0723a3f61501b496b2b752df9795
Munish Soni <48321723+Munish-Soni@users.noreply.github.com>
2024-02-11 18:56:36 +0530

    trying to fix security group 1

--- main.tf
+++ main.tf
@@ -18,13 +18,6 @@ resource "aws_vpc" "default" {
   default = true
 }
 
-resource "aws_security_group" "blog" {
-  name = "blog"
-  description = "Allow http and https in. Allow everything out."
-
-  vpc_id = data.aws_vpc.default.id
-}
-
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
@@ -34,6 +27,13 @@ resource "aws_instance" "blog" {
   tags = {
     Name = "HelloWorld"
   }
+}
+
+resource "aws_security_group" "blog" {
+  name = "blog"
+  description = "Allow http and https in. Allow everything out."
+
+  vpc_id = data.aws_vpc.default.id
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
