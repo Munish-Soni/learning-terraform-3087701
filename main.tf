333b8b9aa93ebff2e5ebff4da8e13cec83d85076
Munish Soni <48321723+Munish-Soni@users.noreply.github.com>
2024-02-14 19:30:17 +0530

    fix 5b77aed

--- main.tf
+++ main.tf
@@ -72,6 +72,10 @@ module "alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
+      my_target = {
+        target_id = aws_instance.blog.id
+        port = 80
+      }
     }
   }
 
